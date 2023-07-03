0f9b8c86a1cde65e9cdeb89639b0b77420e6adf9
zhaleh <zhalehelmi@gmail.com>
2023-07-03 14:56:54 +0330

    hamburger menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.toggleAttribute("data-visible");
+  console.log("ok");
+});
